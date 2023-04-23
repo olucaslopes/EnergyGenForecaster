02da3b9811dd07df1b6d6cac25fb626e931020de
Lucas Lopes <lucaslopesamorim@gmail.com>
2023-04-23 18:48:59 -0300

    Integrate predictions and inputs

--- app.py
+++ app.py
@@ -1,75 +1,97 @@
 import streamlit as st
-from datetime import datetime
-import pandas as pd
+from datetime import datetime, timedelta
 import folium
 from streamlit_folium import st_folium
 from geopy.geocoders import Nominatim
-from utils import predict_city_irradiation
-from functions_models import xgboost_regressor
+from utils import get_irrad_data
+import plotly.express as px
 
 st.set_page_config(page_title='Energy Generation Predictor', layout='wide', page_icon='🔌')
 
 if 'pressed_change_addr' not in st.session_state:
     st.session_state.pressed_change_addr = False
+if 'changed_addr' not in st.session_state:
+    st.session_state.changed_addr = False
 if 'loc_name' not in st.session_state:
     st.session_state.loc_name = 'São Paulo, SP'
 if 'loc_lat' not in st.session_state:
     st.session_state.loc_lat = -23.55
 if 'loc_lon' not in st.session_state:
     st.session_state.loc_lon = -46.64
+if 'panels_area' not in st.session_state:
+    st.session_state.panels_area = 18
+if 'panels_rend' not in st.session_state:
+    st.session_state.panels_rend = 15
+if 'energy_price' not in st.session_state:
+    st.session_state.energy_price = 0.656
 
 st.header('🔌 Energy Generation Predictor')
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+if 'irrad_data' not in st.session_state or st.session_state.changed_addr:
+    st.session_state.changed_addr = False
+    st.session_state.irrad_data = get_irrad_data(st.session_state.loc_lat, st.session_state.loc_lon)
+    # st.session_state.irrad_data = (irrad_data
+    #                                .assign(type=irrad_data['true_label'].replace({0: 'predicted', 1: 'actual'}))
+    #                                .drop(columns='true_value'))
+
+pred_irrad_sum = st.session_state.irrad_data.query('type == "predicted"')['shortwave_radiation_sum'].sum()
+
+# Calculate generated energy
+gen_energy = pred_irrad_sum * st.session_state.panels_area * st.session_state.panels_rend / 100
+savings = gen_energy * st.session_state.energy_price
+
 col_main1, col_main2 = st.columns([3, 1])
 
 with col_main1:
     col_mainsub1, col_mainsub2 = st.columns(2)
     with col_mainsub1:
-        st.metric(label='Total Estimated Generated Energy', value=f'⚡ {18_240:,} kW/h')
+        st.metric(label='Total Estimated Generated Energy', value=f'⚡ {gen_energy:,.0f} kW/h')
 
     with col_mainsub2:
-        st.metric(label='Estimated saving', value=f'💲 R$ {11_965.44:,.2f}')
+        st.metric(label='Estimated saving', value=f'💲 R$ {savings:,.2f}')
 
-    #### TO BE REPLACED ######
-    # # CSV's
-    # df_irrad = pd.read_csv('data/openw_irradiation_2005_2020.csv')
-    # df_irrad.drop(['day', 'weather_id'], axis=1, inplace=True)
-    #
-    # y_pred = xgboost_regressor(df_irrad)  # irradiation predictions
-    #
-    # # y_pred = [118.130264, 114.73563 , 101.42404 , 176.47775 , 112.80841 ,
-    # #        157.99327 , 110.982155, 108.196976, 110.56889 , 137.60085 ,
-    # #        110.87699 , 109.21667 , 102.76564 , 104.48606 , 167.93588 ,
-    # #        109.639755, 188.73145 , 113.77642 , 139.08853 , 114.32364 ,
-    # #        150.87944 , 150.07391 , 167.99814 , 133.70808 , 118.03198 ,
-    # #        133.8792  , 142.81291 , 151.81943 , 145.09813 , 150.08461 ,
-    # #        108.7888  , 154.68489 , 131.83202 , 119.56206 , 156.33952 ,
-    # #        113.300896, 167.98074 , 153.09451 , 132.89561 , 149.40417 ,
-    # #        212.66118 , 115.48822 , 165.70786 , 158.96251 , 104.79509 ,
-    # #        167.0878  ,  96.5487  , 133.39662]
-    #
-    # now = datetime.now()
-    # current_month = now.month
-    # current_year = now.year
-    #
-    # dates = [datetime(current_year + ((current_month + i - 1) // 12), (current_month + i) % 12 or 12, 1) for i in
-    #          range(1, 33)]
-    # df = pd.DataFrame({'data': dates,
-    #                    'irrad kwh/m2': y_pred})
-    #
-    # ##########################################
+    st.subheader('Monthly Potential Generated Energy')
 
-    # st.line_chart(data=df, x='data', y='irrad kwh/m2')
+    ten_years_ago = datetime.today() - timedelta(days=365 * 10)
+
+    st.session_state.irrad_data = st.session_state.irrad_data.assign(
+        kWh=st.session_state.irrad_data['shortwave_radiation_sum'] * st.session_state.panels_area * st.session_state.panels_rend / 100
+    )
+    fig1 = px.line(
+        st.session_state.irrad_data[(st.session_state.irrad_data['time'] >= ten_years_ago)],
+        x='time', y='kWh',
+        color='type',
+        height=300
+    )
+    fig1.update_layout(legend=dict(
+        orientation="h",
+        entrywidth=70,
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    ))
+
+    st.plotly_chart(fig1, theme="streamlit", use_container_width=True)
+
+    # fig1.update_layout(template='plotly_white')
+    # pivot_irrad = (irrad_data[(irrad_data['time'] >= ten_years_ago)]
+    #                .pivot(index='time', values='shortwave_radiation_sum', columns='true_value')
+    #                .rename(columns={1: 'actual', 0: 'predicted'})
+    #                .reset_index())
+
+    # st.line_chart(data=pivot_irrad, x='time', y=['actual', 'predicted'])
 
 with col_main2:
     # user inputs
-    area = st.number_input('📐 Área total em m² dos painéis solares', step=5, min_value=2, value=18)
-    rend = st.slider('💱 Eficiência média dos painéis solares', min_value=0, max_value=100, step=1, format='%d%%',
+    st.session_state.panels_area = st.number_input('📐 Área total em m² dos painéis solares', step=5, min_value=2, value=18)
+    st.session_state.panels_rend = st.slider('💱 Eficiência média dos painéis solares', min_value=0, max_value=100, step=1, format='%d%%',
                      value=15)
-    range_data = st.number_input('🗓️ Anos para previsão', min_value=1, max_value=20, step=1)
+    number = st.number_input('💵 Tarifa média por kWh (R$)', min_value=0.05, max_value=2., step=0.001, value=0.656, format='%.3f')
+    st.session_state.energy_price = number
 
     col_desc_loc, col_alt_loc = st.columns(2)
     with col_desc_loc:
@@ -79,6 +101,7 @@ with col_main2:
 
     if change_addr or st.session_state.pressed_change_addr:
         st.session_state.pressed_change_addr = True
+        st.session_state.changed_addr = True
         loc_input = st.text_input('Digite a localização')
         if loc_input:
             st.session_state.pressed_change_addr = False
@@ -102,11 +125,3 @@ with col_main2:
         [st.session_state.loc_lat, st.session_state.loc_lon]
     ).add_to(m)
     st_folium(m, height=100, width=None)
-
-
-#######################App###################################
-# st.dataframe(df)
-#
-# # calc_energy
-# calc_energy = df['irrad kwh/m2'].sum() * area * rend / 100
-# st.write(f'A energia gerada para os próximos {range_data} anos é', round(calc_energy, 2), 'kwh')
--- utils.py
+++ utils.py
@@ -1,12 +1,19 @@
 from autogluon.timeseries import TimeSeriesPredictor, TimeSeriesDataFrame
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import requests
 import pandas as pd
 
 # Define date ranges for historical irradiation data
 
 today = datetime.today()
-end_date = (datetime.today() - timedelta(days=8)).strftime('%Y-%m-%d')
+if today.day > 9:
+    # last_day_of_last_month
+    end_date = date(today.year, today.month, 1) - timedelta(days=1)
+else:
+    # last day of last 2nd month
+    last_month = date(today.year, today.month, 1) - timedelta(days=1)
+    end_date = date(last_month.year, last_month.month, 1) - timedelta(days=1)
+
 start_date = (today - timedelta(days=365 * 20)).strftime('%Y-%m-%d')
 
 # Load pre-trained time series model
@@ -19,7 +26,8 @@ def predict_city_irradiation(df_):
         id_column="ibge_code",
         timestamp_column="time"
     )
-    return model.predict(city)
+
+    return model.predict(city, model='AutoGluonTabular').reset_index()
 
 
 def get_ibge_code(lat, lon):
@@ -35,7 +43,7 @@ def get_ibge_code(lat, lon):
         raise ValueError('Could not find ibge code for city')
 
 
-def get_irradiation_data(lat, lon):
+def get_historical_data(lat, lon):
     url = (f'https://archive-api.open-meteo.com/v1/archive?latitude={lat}&longitude={lon}'
            f'&start_date={start_date}&end_date={end_date}&daily=weathercode,shortwave_radiation_sum'
            '&timezone=America%2FSao_Paulo')
@@ -43,6 +51,23 @@ def get_irradiation_data(lat, lon):
     response = requests.get(url)
     df = pd.DataFrame(response.json()['daily'])
     ibge_code = get_ibge_code(lat, lon)
-    df['ibge_code'] = ibge_code
+    df = df.assign(ibge_code=ibge_code, time=pd.to_datetime(df['time'], format='%Y-%m-%d'))
     return df
 
+
+def get_irrad_data(lat, lon):
+    irrad_past = get_historical_data(lat, lon)
+
+    monthly_irrad_past = irrad_past.groupby(
+        ['ibge_code', pd.Grouper(key='time', freq='M')]
+    )['shortwave_radiation_sum'].sum().to_frame().reset_index()
+
+    irrad_pred = predict_city_irradiation(monthly_irrad_past)[['timestamp', 'mean']]
+
+    irrad_pred = irrad_pred.rename(
+        columns={'timestamp': 'time', 'mean': 'shortwave_radiation_sum'}
+    ).assign(type='predicted')
+
+    irrad_past = monthly_irrad_past[['time', 'shortwave_radiation_sum']].assign(type='actual')
+
+    return pd.concat([irrad_past, irrad_pred])
