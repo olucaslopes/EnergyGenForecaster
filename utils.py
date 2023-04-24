1880c2aa3c7de67c51ab24c1131789861ed35139
Lucas Lopes <lucaslopesamorim@gmail.com>
2023-04-24 20:33:40 -0300

    Refactor utils.py

--- utils.py
+++ utils.py
@@ -4,26 +4,37 @@ import requests
 import pandas as pd
 import os
 
-# Define date ranges for historical irradiation data
+# Define constants for readability
+LAST_DAY_OF_MONTH = 9
+YEARS_OF_HISTORICAL_DATA = 20
 
+# Define date ranges for historical irradiation data
 today = datetime.today()
-if today.day > 9:
-    # last_day_of_last_month
+if today.day > LAST_DAY_OF_MONTH:
     end_date = date(today.year, today.month, 1) - timedelta(days=1)
 else:
-    # last day of last 2nd month
     last_month = date(today.year, today.month, 1) - timedelta(days=1)
     end_date = date(last_month.year, last_month.month, 1) - timedelta(days=1)
 
-start_date = (today - timedelta(days=365 * 20)).strftime('%Y-%m-%d')
+start_date = (today - timedelta(days=365 * YEARS_OF_HISTORICAL_DATA)).strftime('%Y-%m-%d')
 
 # Load pre-trained time series model
-full_path = os.path.abspath("./autogluon-m4-monthly")
-model = TimeSeriesPredictor.load(full_path)
+MODEL_PATH = os.path.abspath("./autogluon-m4-monthly")
+model = TimeSeriesPredictor.load(MODEL_PATH)
 
-def predict_city_irradiation(df_):
+
+def predict_city_irradiation(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Predict the shortwave radiation sum for a city using a pre-trained time series model.
+
+    Args:
+        df: A pandas DataFrame containing historical shortwave radiation sum data.
+
+    Returns:
+        A pandas DataFrame containing the predicted shortwave radiation sum for the given city.
+    """
     city = TimeSeriesDataFrame.from_data_frame(
-        df_,
+        df,
         id_column="ibge_code",
         timestamp_column="time"
     )
@@ -31,7 +42,17 @@ def predict_city_irradiation(df_):
     return model.predict(city, model='AutoGluonTabular').reset_index()
 
 
-def get_ibge_code(lat, lon):
+def get_ibge_code(lat: float, lon: float) -> str:
+    """
+    Get the IBGE code for the municipality located at the given latitude and longitude.
+
+    Args:
+        lat: The latitude of the municipality.
+        lon: The longitude of the municipality.
+
+    Returns:
+        The IBGE code of the municipality as a string.
+    """
     url = f'http://servicodados.ibge.gov.br/api/v1/localidades/municipios?lat={lat}&lon={lon}'
 
     response = requests.get(url, verify=False)
@@ -44,19 +65,41 @@ def get_ibge_code(lat, lon):
         raise ValueError('Could not find ibge code for city')
 
 
-def get_historical_data(lat, lon):
+def get_historical_data(lat: float, lon: float) -> pd.DataFrame:
+    """
+    Get historical irradiation data for a given location.
+
+    Args:
+        lat: The latitude of the location.
+        lon: The longitude of the location.
+
+    Returns:
+        A pandas DataFrame containing historical irradiation data.
+    """
     url = (f'https://archive-api.open-meteo.com/v1/archive?latitude={lat}&longitude={lon}'
            f'&start_date={start_date}&end_date={end_date}&daily=weathercode,shortwave_radiation_sum'
            '&timezone=America%2FSao_Paulo')
 
     response = requests.get(url)
+    response.raise_for_status()
+
     df = pd.DataFrame(response.json()['daily'])
     ibge_code = get_ibge_code(lat, lon)
     df = df.assign(ibge_code=ibge_code, time=pd.to_datetime(df['time'], format='%Y-%m-%d'))
     return df
 
 
-def get_irrad_data(lat, lon):
+def get_irrad_data(lat: float, lon: float) -> pd.DataFrame:
+    """
+    Get actual and predicted shortwave radiation sum data for a given location.
+
+    Args:
+        lat: The latitude of the location.
+        lon: The longitude of the location.
+
+    Returns:
+        A pandas DataFrame containing actual and predicted shortwave radiation sum data for the given location.
+    """
     irrad_past = get_historical_data(lat, lon)
 
     monthly_irrad_past = irrad_past.groupby(
